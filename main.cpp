3d1add24b74cbd8ae8a4dc12fba1837b112cd2f9
siyuxiaoxiao <siyuxiaoxiao@outlook.com>
2018-11-04 16:00:34 +0800

    第一次更新 增加系统菜单

--- main.cpp
+++ main.cpp
@@ -3,24 +3,30 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <fstream>
 /*
  * 2018-11-04
  * 张贺
  * 学生成绩管理
  * */
 using namespace std;
-class Course{
-public:
 
+class Course {
+public:
+    virtual ~Course() = 0;
 };
 
-class Math:public Course{
+Course::~Course() {}
+
+class Math : public Course {
 private:
     double score;
 public:
-    Math(){}
-    Math(double _score):score(_score){}
-    double getscore() const { return score;}
+    Math() {}
+
+    Math(double _score) : score(_score) {}
+
+    double getscore() const { return score; }
 
     double getScore() const {
         return score;
@@ -31,25 +37,32 @@ public:
     }
 
 };
-class Physics:public Course{
+
+class Physics : public Course {
 private:
     double score;
 public:
-    Physics(){}
-    Physics(double _score):score(_score){}
-    double getscore() const { return score;}
-    void set(double score){this->score= score;}
+    Physics() {}
+
+    Physics(double _score) : score(_score) {}
+
+    double getscore() const { return score; }
+
+    void set(double score) { this->score = score; }
 };
-class  Score{
+
+class Score {
 private:
     Math math;
     Physics physics;
 public:
-    Score(){}
-    Score(Math _math,Physics _physice):math(_math),physics(_physice){}
-    void setScore(Math _math,Physics _physice){
-        math=_math;
-        physics=_physice;
+    Score() {}
+
+    Score(Math _math, Physics _physice) : math(_math), physics(_physice) {}
+
+    void setScore(Math _math, Physics _physice) {
+        math = _math;
+        physics = _physice;
     }
 
     const double getMath() const {
@@ -68,15 +81,18 @@ public:
         Score::physics = physics;
     }
 };
-class Student{
+
+class Student {
 private:
     string name;
-    string   num;
+    string num;
     Score score;
     double average;
 public:
-    Student(){}
-    Student(string _name,string _num):name(_name),num(_num){}
+    Student() {}
+
+    Student(string _name, string _num) : name(_name), num(_num) {}
+
     const string &getName() const {
         return name;
     }
@@ -92,12 +108,14 @@ public:
     void setNum(string num) {
         Student::num = num;
     }
-    void setScore(double math, double phycise){
-        this->score.setScore(math,phycise);
-        this->average=(math+phycise)/2;
+
+    void setScore(double math, double phycise) {
+        this->score.setScore(math, phycise);
+        this->average = (math + phycise) / 2;
     }
-    Score getSocre(){
-        return  score;
+
+    Score getSocre() {
+        return score;
     }
 
     double getAverage() const {
@@ -105,125 +123,209 @@ public:
     }
 
 };
-class ScoreManager{
+
+class ScoreManager {
 private:
     vector<Student> students;
 public:
-    ScoreManager(){};
+    ScoreManager() {};
 
-    void addStudent(string name,string num);
+    ~ScoreManager();
+
+    void addStudent(string name, string num);
+
     void delStudent(string name);
-    void setStudentScore(string num,double math, double physice);
+
+    void setStudentScore(string num, double math, double physice);
+
     void printStudentByNum(string num);
+
     void sortByCourse();
+
     void sortByAve();
+
     void statisAve();
+
     void PrintStudentInfo();
+
     void printAllStudentInfo();
 };
+
+ScoreManager::~ScoreManager() {
+    ofstream outfile("data.txt");
+    cout << "保存中...." << endl;
+    for (Student student:students) {
+        outfile << student.getName() << "\t" << student.getNum() << "\t" << student.getSocre().getMath() << "\t\t"
+                << student.getSocre().getPhysics() << endl;
+    }
+    cout << "保存成功" << endl;
+}
+
 void ScoreManager::PrintStudentInfo() {
-    for (Student student:students){
-        cout<<student.getName()<<"   "<<student.getNum()<<"  "<<endl;
+    for (Student student:students) {
+        cout << student.getName() << "   " << student.getNum() << "  " << endl;
     }
 }
-void ScoreManager::addStudent(string name,string num) {
-    Student student(name,num);
+
+void ScoreManager::addStudent(string name, string num) {
+    Student student(name, num);
     students.push_back(student);
 }
+
 void ScoreManager::delStudent(string num) {
-    int flag=0;
-    for(Student student:students){
-        if(student.getNum()==num){
+    int flag = 0;
+    for (Student student:students) {
+        if (student.getNum() == num) {
             break;
         }
         flag++;
     }
-    students.erase(students.begin()+flag);
+    students.erase(students.begin() + flag);
+    cout << "删除成功" << endl;
 }
-void ScoreManager::setStudentScore(string num,double math, double physice) {
-    int flag=0;
-    for(Student student:students){
-        if(student.getNum()==num){
+
+void ScoreManager::setStudentScore(string num, double math, double physice) {
+    int flag = 0;
+    for (Student student:students) {
+        if (student.getNum() == num) {
             break;
         }
         flag++;
     }
-    students[flag].setScore(math,physice);
+    students[flag].setScore(math, physice);
+    cout << math << "  " << physice << endl;
 }
+
 void ScoreManager::printStudentByNum(string num) {
 
-    for(Student student:students){
-        if(student.getNum()==num){
-            cout<<"姓名"<<"\t"<<"学号"<<"\t\t"<<"数学成绩"<<"\t"<<"物理成绩"<<endl;
-            cout<<student.getName()<<"\t"<<student.getNum()<<"\t"<<student.getSocre().getMath()<<"\t\t"<<student.getSocre().getPhysics()<<endl;
+    for (Student student:students) {
+        if (student.getNum() == num) {
+            cout << "姓名" << "\t" << "学号" << "\t\t" << "数学成绩" << "\t" << "物理成绩" << endl;
+            cout << student.getName() << "\t" << student.getNum() << "\t" << student.getSocre().getMath() << "\t\t"
+                 << student.getSocre().getPhysics() << endl;
         }
     }
 }
+
 void ScoreManager::printAllStudentInfo() {
-    cout<<"姓名"<<"\t"<<"学号"<<"\t\t"<<"数学成绩"<<"\t"<<"物理成绩\t"<<"平均成绩"<<endl;
-    for(Student student:students){
-        cout<<student.getName()<<"\t"<<student.getNum()<<"\t"<<student.getSocre().getMath()<<"\t\t"<<student.getSocre().getPhysics()<<"\t\t"<<student.getAverage()<<endl;
+    cout << "姓名" << "\t" << "学号" << "\t\t" << "数学成绩" << "\t" << "物理成绩\t" << "平均成绩" << endl;
+    for (Student student:students) {
+        cout << student.getName() << "\t" << student.getNum() << "\t" << student.getSocre().getMath() << "\t\t"
+             << student.getSocre().getPhysics() << "\t\t" << student.getAverage() << endl;
     }
 }
 
 void ScoreManager::sortByCourse() {
-    cout<<"请输入排序科目  物理 or 数学"<<endl;
+    cout << "请输入排序科目  物理 or 数学" << endl;
     string chiose;
-    cin>>chiose;
-    if(chiose=="物理"){
-        sort(students.begin(),students.end(),[](Student &s1,Student s2){
-            return s1.getSocre().getPhysics()>s2.getSocre().getPhysics();
+    cin >> chiose;
+    if (chiose == "物理") {
+        sort(students.begin(), students.end(), [](Student &s1, Student s2) {
+            return s1.getSocre().getPhysics() > s2.getSocre().getPhysics();
         });
-        cout<<"-----------------------物理成绩排名------------------"<<endl;
+        cout << "-----------------------物理成绩排名------------------" << endl;
         printAllStudentInfo();
     }
-    if(chiose=="数学"){
-        sort(students.begin(),students.end(),[](Student &s1,Student s2){
-            return s1.getSocre().getMath()>s2.getSocre().getMath();
+    if (chiose == "数学") {
+        sort(students.begin(), students.end(), [](Student &s1, Student s2) {
+            return s1.getSocre().getMath() > s2.getSocre().getMath();
         });
-        cout<<"-----------------------数学成绩排名------------------"<<endl;
+        cout << "-----------------------数学成绩排名------------------" << endl;
         printAllStudentInfo();
     }
 
 }
+
 void ScoreManager::sortByAve() {
-    sort(students.begin(),students.end(),[](Student &s1,Student s2){
-        return s1.getAverage()>s2.getAverage();
+    sort(students.begin(), students.end(), [](Student &s1, Student s2) {
+        return s1.getAverage() > s2.getAverage();
     });
-    cout<<"-----------------------平均成绩排名------------------"<<endl;
+    cout << "-----------------------平均成绩排名------------------" << endl;
     printAllStudentInfo();
 }
+
 void ScoreManager::statisAve() {
-    double mathAverage=0;
-    double physicsAverage=0;
-    double mathSum=0;
-    double physiceSum=0;
-    for(Student student:students){
-        mathSum+=student.getSocre().getMath();
-        physiceSum+=student.getSocre().getPhysics();
+    double mathAverage = 0;
+    double physicsAverage = 0;
+    double mathSum = 0;
+    double physiceSum = 0;
+    for (Student student:students) {
+        mathSum += student.getSocre().getMath();
+        physiceSum += student.getSocre().getPhysics();
     }
-    physicsAverage=physiceSum/students.size();
-    mathAverage=mathSum/students.size();
-    cout<<"-----------------------平均分统计------------------"<<endl;
-    cout<<"数学平均分"<<"\t"<<"物理平均分"<<endl;
-    cout<<mathAverage<<"\t\t"<<physicsAverage<<endl;
+    physicsAverage = physiceSum / students.size();
+    mathAverage = mathSum / students.size();
+    cout << "-----------------------平均分统计------------------" << endl;
+    cout << "数学平均分" << "\t" << "物理平均分" << endl;
+    cout << mathAverage << "\t\t" << physicsAverage << endl;
 }
+
 int main() {
     ScoreManager scoreManager;
-    scoreManager.addStudent("张贺","201512300045");
-    scoreManager.addStudent("臭宝宝","201512300037");
-    scoreManager.addStudent("香宝宝","201512300031");
-    scoreManager.addStudent("酸宝宝","201512300032");
-    scoreManager.addStudent("苦宝宝","201512300033");
-    scoreManager.addStudent("辣宝宝","201512300034");
-    cout<<"-----------------------------"<<endl;
-    scoreManager.setStudentScore("201512300045",99,99);
-    scoreManager.setStudentScore("201512300037",98,93);
-    scoreManager.setStudentScore("201512300031",67,12);
-    scoreManager.setStudentScore("201512300032",76,17);
-    scoreManager.setStudentScore("201512300033",33,78);
-    scoreManager.setStudentScore("201512300034",87,91);
-    scoreManager.sortByAve();
-    scoreManager.statisAve();
-    return 0;
+    cout << "换用使用学生管理系统" << endl;
+    while (true) {
+        cout << "-----------------请选择功能-------------------" << endl;
+        cout << "0.  增加学生" << endl;
+        cout << "1.  删除学生" << endl;
+        cout << "2.  设置学生成绩" << endl;
+        cout << "3.  输出指定学生成绩" << endl;
+        cout << "4.  按课程分类进行排序" << endl;
+        cout << "5.  按学生成绩平均分排序" << endl;
+        cout << "6.  统计各科成绩平均分" << endl;
+        cout << "7.  退出并保存" << endl;
+        int choise;
+        cin >> choise;
+        switch (choise) {
+            case 0: {
+                scoreManager.addStudent("张贺", "201512300045");
+                scoreManager.addStudent("臭宝宝", "201512300037");
+                scoreManager.addStudent("香宝宝", "201512300031");
+                scoreManager.addStudent("酸宝宝", "201512300032");
+                scoreManager.addStudent("苦宝宝", "201512300033");
+                scoreManager.addStudent("辣宝宝", "201512300034");
+                break;
+            }
+            case 1: {
+                cout << "请输入删除学生学号" << endl;
+                string num;
+                cin >> num;
+                scoreManager.delStudent(num);
+                break;
+            }
+            case 2: {
+                scoreManager.setStudentScore("201512300045", 99, 99);
+                scoreManager.setStudentScore("201512300037", 98, 93);
+                scoreManager.setStudentScore("201512300031", 67, 12);
+                scoreManager.setStudentScore("201512300032", 76, 17);
+                scoreManager.setStudentScore("201512300033", 33, 78);
+                scoreManager.setStudentScore("201512300034", 87, 91);
+                break;
+            }
+            case 3: {
+                cout << "请输入查找学生学号" << endl;
+                string num;
+                cin >> num;
+                scoreManager.printStudentByNum(num);
+                break;
+            }
+            case 4: {
+                scoreManager.sortByCourse();
+                break;
+            }
+            case 5: {
+                scoreManager.sortByCourse();
+                break;
+            }
+            case 6: {
+                scoreManager.statisAve();
+                break;
+            }
+            case 7: {
+                return 0;
+            }
+        }
+
+
+    }
+
 }
